c0d572207a9066b77ef1747c78428a047554264e
Reese Wilson <reese@shinymayhem.com>
2015-03-27 17:52:04 +0000

    integrate patient id incrementer

--- src/index.js
+++ src/index.js
@@ -2,60 +2,67 @@ var http = require("http");
 var express = require("express");
 var app = express();
 var getRawBody = require('raw-body');
-var req = require('request');
+var request = require('request');
 var apiUrl = process.env.API_URL;
 var apiBase = "/rest/aera-ct";
 
-app.get("/settings/download/:serial", function(request, response, next) {
+app.get("/settings/download/:serial", function(req, res, next) {
 	console.log("overriding settings download");
-	var serial = request.params.serial;
-	var settings = {
-		121: "01"
-	};
-	json = JSON.stringify(settings);
-	console.log("settings json", json);
-	var requestOptions = {
-		host: apiUrl,
-		port: 80,
-		path: apiBase + "/settings/set/" + serial,
-		method: 'PUT',
-		headers: {
-			'Content-Length': json.length,
-			'Content-Type': 'application/json'
-		}
-	};
-	var override = http.request(requestOptions, function(res) {
-		console.log("inside request override");
-		res.setEncoding('utf8');
-		res.on('error', function(err) {
-			console.log('override error:', err);
-			response.status(500);
-			response.json({error: "Something went wrong"});
+	var serial = req.params.serial;
+	function onGetPatientId(err, response, body) {
+		console.log("body:", body);
+		var patientId = parseInt(body);
+		console.log("patientId:", patientId);
+		var settings = {
+			patientId: patientId,
+			wirelessEnable: false
+		};
+		json = JSON.stringify(settings);
+		console.log("settings json", json);
+		var requestOptions = {
+			host: apiUrl,
+			port: 80,
+			path: apiBase + "/settings/set/" + serial,
+			method: 'PUT',
+			headers: {
+				'Content-Length': json.length,
+				'Content-Type': 'application/json'
+			}
+		};
+		var override = http.request(requestOptions, function(res) {
+			console.log("inside request override");
+			res.setEncoding('utf8');
+			res.on('error', function(err) {
+				console.log('override error:', err);
+				res.status(500);
+				res.json({error: "Something went wrong"});
+			});
+			res.on('data', function(chunk) {
+				console.log('override response:', chunk);
+				console.log('next');
+				next();
+			});
 		});
-		res.on('data', function(chunk) {
-			console.log('override response:', chunk);
-			console.log('next');
-			next();
+		override.on('error', function(e) {
+			console.log("override request error:", e);
+			res.status(500);
+			res.json({error: "Something went wrong"});
 		});
-	});
-	override.on('error', function(e) {
-		console.log("override request error:", e);
-		response.status(500);
-		response.json({error: "Something went wrong"});
-	});
-	override.write(json);
-	override.end();
+		override.write(json);
+		override.end();
+	}
+	request("http://increment/patientId", onGetPatientId);
 });
 
-app.use(function(request, response, next) {
+app.use(function(req, res, next) {
 	console.log("Got request");
 	console.log("apiUrl", apiUrl);
-	console.log("request.url", request.url);
-	var url = "http://" + apiUrl + request.url;
-	var inner = req(url);
-	var piped = request.pipe(inner);
-	var res = piped.pipe(response);
-	//request.pipe(req(url)).pipe(response);
+	console.log("request.url", req.url);
+	var url = "http://" + apiUrl + req.url;
+	var inner = request(url);
+	var piped = req.pipe(inner);
+	var res = piped.pipe(res);
+	//req.pipe(request(url)).pipe(res);
 });
 
 console.log("starting server");
